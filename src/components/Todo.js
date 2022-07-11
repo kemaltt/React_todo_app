83794e08f392e5ed920334bf0b09ccf2a3a4a241
kemaltt <ttnckemal@gmail.com>
2022-07-11 15:54:09 +0200

    ready

---
+++ src/components/Todo.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function Todo({ i, todo, counter, setCounter }) {
+  const [isTodoDone, setIsTodoDone] = useState(true);
+
+  const todoDone = () => {
+    setIsTodoDone(!isTodoDone);
+
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <ul style={{}}>
+        <li
+          style={{
+            textDecoration: !isTodoDone ? "line-through" : "",
+            backgroundColor: !isTodoDone ? "yellowgreen" : "",
+          }}
+          key={i}
+          onClick={todoDone}
+        >
+          {todo}
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import Todo from "../components/Todo";
+
+export default function Home(props) {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    if (input.length > 0) {
+      setTodos([...todos, input]);
+      setCounter(counter + 1);
+      setInput("");
+    } else {
+      alert("Please enter a todo");
+    }
+  };
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="todo_container">
+      <div className="todos_input">
+        <h1>Todo App</h1>
+        <p>{counter}</p>
+        <form onSubmit={addTodo} action="">
+          <input
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            value={input}
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Neues TODO"
+          />
+          <button type="submit">+</button>
+        </form>
+      </div>
+      <div className="todo_list">
+        {todos.map((todo, i) => (
+          <Todo key={i} todo={todo} counter={counter} setCounter={setCounter} />
+        ))}
+      </div>
+    </div>
+  );
+}
